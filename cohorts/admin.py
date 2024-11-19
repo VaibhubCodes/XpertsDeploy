7076a7d56f21ee1cdb64d898d635baab265922be
Hometasks <vaibhav@vedspace.in>
2024-11-20 01:25:46 +0530

    Update admin.py

--- cohorts/admin.py
+++ cohorts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Cohort, CohortRegistration, Payment
 
+class CohortBaseAdmin(admin.ModelAdmin):
+    """Base Admin class to define shared logic."""
+    
+    def get_cohort_name(self, obj):
+        """Returns the name of the cohort."""
+        return obj.cohort.name if hasattr(obj, 'cohort') else obj.cohort_registration.cohort.name
+    get_cohort_name.short_description = 'Cohort'
+
+
 @admin.register(Cohort)
 class CohortAdmin(admin.ModelAdmin):
     list_display = ('name', 'mentor', 'start_date', 'end_date', 'price')
@@ -8,36 +17,30 @@ class CohortAdmin(admin.ModelAdmin):
     list_filter = ('start_date', 'end_date', 'price')
     ordering = ('-start_date',)
 
+
 @admin.register(CohortRegistration)
-class CohortRegistrationAdmin(admin.ModelAdmin):
+class CohortRegistrationAdmin(CohortBaseAdmin):
     list_display = ('get_cohort_name', 'user', 'get_mentor_name', 'registered_at', 'payment_status')
     search_fields = ('user__username', 'cohort__name')
     list_filter = ('payment_status', 'registered_at')
     ordering = ('-registered_at',)
-    autocomplete_fields = ['cohort', 'user']  # Cohort first, then user
-
-    # Define the order of fields in the admin form
+    autocomplete_fields = ['cohort', 'user']
     fields = ['cohort', 'user', 'payment_status']
 
-    def get_cohort_name(self, obj):
-        return obj.cohort.name
-    get_cohort_name.short_description = 'Cohort'
-
     def get_mentor_name(self, obj):
+        """Returns the mentor's username."""
         return obj.cohort.mentor.username
     get_mentor_name.short_description = 'Mentor'
 
+
 @admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('get_cohort_name', 'user', 'amount', 'payment_status', 'payment_date', 'razorpay_order_id', 'razorpay_payment_id', 'verified')
-    search_fields = ('user__username', 'cohort_registration__cohort__name', 'razorpay_order_id', 'razorpay_signature')
+class PaymentAdmin(CohortBaseAdmin):
+    list_display = ('get_cohort_name', 'user', 'amount', 'payment_status', 'payment_date', 
+                    'razorpay_order_id', 'razorpay_payment_id', 'verified')
+    search_fields = ('user__username', 'cohort_registration__cohort__name', 
+                     'razorpay_order_id', 'razorpay_signature')
     list_filter = ('payment_status', 'payment_date', 'verified')
     ordering = ('-payment_date',)
-    autocomplete_fields = ['cohort_registration', 'user']  # Cohort Registration first, then user
-
-    # Define the order of fields in the admin form, excluding razorpay_signature and payment_date since they are non-editable
-    fields = ['cohort_registration', 'user', 'amount', 'razorpay_order_id', 'razorpay_payment_id', 'payment_status', 'verified']
-
-    def get_cohort_name(self, obj):
-        return obj.cohort_registration.cohort.name
-    get_cohort_name.short_description = 'Cohort'
+    autocomplete_fields = ['cohort_registration', 'user']
+    fields = ['cohort_registration', 'user', 'amount', 'razorpay_order_id', 
+              'razorpay_payment_id', 'payment_status', 'verified']
